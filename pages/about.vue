07d3a092294feae19d011d8fc85aebdab14767f6
jay <jab@globalia.com>
2023-02-06 15:36:33 +0530

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis nulla minus architecto optio porro illum
+            vitae, repudiandae dolores ratione. Aperiam tenetur corporis deserunt excepturi enim molestiae perferendis
+            optio nisi ea.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum, minima, quia esse consequatur molestiae eos
+            dolore repellat tempora unde exercitationem provident! Similique ratione ut labore vel sint dolor minus
+            assumenda?</p>
+        <div>{{ data }}</div>
+    </div>
+</template>
+
+<script setup>
+const { data } = await useFetch('/api/currency/GBP')
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse accusantium in soluta earum mollitia
+            dignissimos consequatur nihil animi dicta suscipit velit distinctio, rem deserunt, voluptatem assumenda
+            recusandae consequuntur aliquid adipisci!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium est accusamus, voluptates perspiciatis
+            mollitia vel. Quis quam est facere? Tenetur, magnam voluptatum quis reprehenderit id necessitatibus nisi
+            laudantium vitae quos?</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
